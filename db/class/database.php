e6c7ac13f9fe627b06d91521293f65a4804bd891
Pajer3 <musieatsbeha633@gmail.com>
2025-03-28 12:09:00 +0100

    Refactor database handling: remove obsolete database.php, introduce new class structure in class/database.php, and enhance connection management with PDO. Update environment configuration to include charset for improved database interactions.

---
+++ db/class/database.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../..');
+$dotenv->load();
+
+class Database {
+    private $servername;
+    private $username;
+    private $password;
+    private $database;
+    private $charset;
+    private $dsn;
+
+
+    public function __construct() {
+        $this->servername = $_ENV['servername'];
+        $this->username = $_ENV['username'];
+        $this->password = $_ENV['password'];
+        $this->database = $_ENV['database'];
+        $this->charset = $_ENV['charset'];
+        $this->dsn = "mysql:host={$this->servername};dbname={$this->database};charset={$this->charset}";
+    }
+
+    public function connect() {
+        try {
+            $conn = new PDO(
+                $this->dsn,
+           $this->username, 
+           $this->password, 
+                [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                          PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                          PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES {$charset}"
+                          ]
+            );
+            
+        }
+    }
+
+    public function close($conn) {
+        $conn->close();
+    }
+}
