806e18b9a22126f3eb14ee423c6dc29ea0e9e6dd
Colin Poort <581827@edu.rocmn.nl>
2025-04-14 12:17:11 +0200

    links header fixed

--- frontend/includes/header.php
+++ frontend/includes/header.php
@@ -2,11 +2,11 @@
         <div class="navbar-container">
             <div class="navbar">
                 <div class="logo">
-                    <a href="#"><img src="../../assets/img/logo.png" alt="Vakantie Villas Logo"></a>
+                    <a href="homepage.php"><img src="../../assets/img/logo.png" alt="Vakantie Villas Logo"></a>
                 </div>
                 <nav>
                     <ul class="nav-links">
-                        <li><a href="homepage.php">Woningen</a></li>
+                        <li><a href="woningen.php">Woningen</a></li>
                         <li><a href="overons.php">Ons</a></li>
                         <li><a href="contact.php">Contact</a></li>
                         <li><a href="register.php" class="register-btn">Register</a></li>
